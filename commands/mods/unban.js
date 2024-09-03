e6fdbbe5ac8fdd199a61667b3c9b38ae89d02871
Itachinmkz <96137239+Itachinmkz@users.noreply.github.com>
2024-09-03 04:24:52 +0200

    Update unban.js

--- commands/mods/unban.js
+++ commands/mods/unban.js
@@ -47,7 +47,7 @@ module.exports = {
                                 //.setTitle(`<:protection:847072581382438953> Modération • Type: **\`bannissement\`**`)
                                 //.setTimestamp()
                                 //.setDescription(` **Bannissement de**: ${user}\n**Auteur**: ${message.author} \n**Salon**: ${message.channel}\n**Temps de réponse**: ${client.ws.ping}ms`)
-                                .setDescription(`${message.author} a **unban** tout les membres bannis`))
+                                .setDescription(`${message.author} a **unban** tous les membres bannis`))
 
                         }
                     })
